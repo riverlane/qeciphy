f9e1af826641d03773ac802b64de9837a2292cdd
Aniket Datta <aniket.connect@gmail.com>
2025-10-12 14:02:37 +0100

    Added lib module: riv_async_fifo

---
+++ lib/riv_async_fifo/src/riv_async_fifo.sv
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: None
+// Copyright (c) 2024-2025 Riverlane Ltd.
+// Original authors: aniketEng
+
+// This module implements an asynchronous FIFO using LUTRAM.  
+// The architecture itself does not impose constraints on the frequency range of WCLK and RCLK.
+//
+// Parameters:
+//
+// 1. DATA_WIDTH (int): Defines the data width of the FIFO. 
+// 2. ADDR_WIDTH (int): Specifies the FIFO depth, where depth = 2**ADDR_WIDTH - 1.
+
+`ifndef RIV_ASYNC_FIFO_SV
+`define RIV_ASYNC_FIFO_SV
+
+module riv_async_fifo #(
+    parameter DATA_WIDTH = 32,
+    parameter ADDR_WIDTH = 10
+) (
+    input  logic                  wclk,
+    input  logic                  wrst_n,
+    input  logic                  wen,
+    input  logic [DATA_WIDTH-1:0] wdata,
+    input  logic                  rclk,
+    input  logic                  rrst_n,
+    input  logic                  ren,
+    output logic [DATA_WIDTH-1:0] rdata,
+    output logic                  full,
+    output logic                  empty,
+    output logic                  overflow,
+    output logic                  underflow,
+    output logic [ADDR_WIDTH-1:0] wwcount,
+    output logic [ADDR_WIDTH-1:0] rwcount
+);
+
+   // -------------------------------------------------------------
+   // Declaration
+   // -------------------------------------------------------------
+
+   logic [ADDR_WIDTH-1:0] waddr;
+   logic [ADDR_WIDTH-1:0] waddr_rd;
+
+   logic [ADDR_WIDTH-1:0] raddr;
+   logic [ADDR_WIDTH-1:0] raddr_wr;
+
+   logic wr_fsm_load;
+   logic wr_fsm_req_ack;
+   logic wr_fsm_recv_ack;
+
+   logic rd_fsm_load;
+   logic rd_fsm_req_ack;
+   logic rd_fsm_recv_ack;
+
+   // -------------------------------------------------------------
+   // Write Control
+   // -------------------------------------------------------------
+
+   riv_async_fifo_ctl #(
+       .ADDR_WIDTH(ADDR_WIDTH)
+   ) i_fifo_wctl (
+       .clk         (wclk),
+       .rst_n       (wrst_n),
+       .en          (wen),
+       .addr        (waddr),
+       .fsm_load    (wr_fsm_load),
+       .fsm_req_ack (wr_fsm_req_ack),
+       .fsm_recv_ack(wr_fsm_recv_ack)
+   );
+
+   // -------------------------------------------------------------
+   // Read Control
+   // -------------------------------------------------------------
+
+   riv_async_fifo_ctl #(
+       .ADDR_WIDTH(ADDR_WIDTH)
+   ) i_fifo_rctl (
+       .clk         (rclk),
+       .rst_n       (rrst_n),
+       .en          (ren),
+       .addr        (raddr),
+       .fsm_load    (rd_fsm_load),
+       .fsm_req_ack (rd_fsm_req_ack),
+       .fsm_recv_ack(rd_fsm_recv_ack)
+   );
+
+   // -------------------------------------------------------------
+   // Memory
+   // -------------------------------------------------------------
+
+   riv_async_fifo_mem #(
+       .ADDR_WIDTH(ADDR_WIDTH),
+       .DATA_WIDTH(DATA_WIDTH)
+   ) i_fifo_mem (
+       .wclk (wclk),
+       .wen  (wen),
+       .waddr(waddr),
+       .wdata(wdata),
+       .raddr(raddr),
+       .rdata(rdata)
+   );
+
+   // -------------------------------------------------------------
+   // CDC
+   // -------------------------------------------------------------
+
+   riv_async_fifo_cdc #(
+       .ADDR_WIDTH(ADDR_WIDTH)
+   ) i_fifo_cdc (
+       .wclk           (wclk),
+       .wrst_n         (wrst_n),
+       .wr_fsm_load    (wr_fsm_load),
+       .wr_fsm_req_ack (wr_fsm_req_ack),
+       .wr_fsm_recv_ack(wr_fsm_recv_ack),
+       .waddr          (waddr),
+       .raddr_wr       (raddr_wr),
+
+       .rclk           (rclk),
+       .rrst_n         (rrst_n),
+       .rd_fsm_load    (rd_fsm_load),
+       .rd_fsm_req_ack (rd_fsm_req_ack),
+       .rd_fsm_recv_ack(rd_fsm_recv_ack),
+       .raddr          (raddr),
+       .waddr_rd       (waddr_rd)
+   );
+
+   // -------------------------------------------------------------
+   // Word count in the FIFO
+   // -------------------------------------------------------------
+
+   // waddr and raddr are treated as unsigned signals.
+   // Even for waddr < raddr, the result will be positive because of wrapping
+
+   // Word count in RCLK domain
+   always_ff @(posedge rclk) begin
+      rwcount <= waddr_rd - raddr;
+   end
+
+   // Word count in WCLK domain
+   always_ff @(posedge wclk) begin
+      wwcount <= waddr - raddr_wr;
+   end
+
+   // -------------------------------------------------------------
+   // Output signal generation
+   // -------------------------------------------------------------
+
+   assign full = (waddr == (raddr_wr - ADDR_WIDTH'(1'b1)));
+   assign empty = (waddr_rd == raddr);
+   assign overflow = wen && full;
+   assign underflow = ren && empty;
+
+endmodule  // riv_async_fifo
+
+`endif  // RIV_ASYNC_FIFO_SV
---
+++ lib/riv_async_fifo/src/riv_async_fifo_cdc.sv
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: None
+// Copyright (c) 2025 Riverlane Ltd.
+// Original authors: aniketEng
+
+`ifndef RIV_ASYNC_FIFO_CDC_SV
+`define RIV_ASYNC_FIFO_CDC_SV
+
+module riv_async_fifo_cdc #(
+    parameter ADDR_WIDTH = 10
+) (
+    input  logic                  wclk,
+    input  logic                  wrst_n,
+    input  logic                  wr_fsm_load,
+    input  logic                  wr_fsm_req_ack,
+    output logic                  wr_fsm_recv_ack,
+    input  logic [ADDR_WIDTH-1:0] waddr,
+    output logic [ADDR_WIDTH-1:0] raddr_wr,
+
+    input  logic                  rclk,
+    input  logic                  rrst_n,
+    input  logic                  rd_fsm_load,
+    input  logic                  rd_fsm_req_ack,
+    output logic                  rd_fsm_recv_ack,
+    input  logic [ADDR_WIDTH-1:0] raddr,
+    output logic [ADDR_WIDTH-1:0] waddr_rd
+);
+
+   // -------------------------------------------------------------
+   // Declaration
+   // -------------------------------------------------------------
+
+   logic [ADDR_WIDTH-1:0] waddr_rd_nxt;
+   logic [ADDR_WIDTH-1:0] waddr_latch;
+   logic [ADDR_WIDTH-1:0] waddr_latch_nxt;
+
+   logic wr_fsm_req_ack_rd;
+   logic waddr_sample_rd;
+   logic wr_fsm_recv_ack_rd;
+
+   logic [ADDR_WIDTH-1:0] raddr_wr_nxt;
+   logic [ADDR_WIDTH-1:0] raddr_latch;
+   logic [ADDR_WIDTH-1:0] raddr_latch_nxt;
+
+   logic rd_fsm_req_ack_wr;
+   logic raddr_sample_wr;
+   logic rd_fsm_recv_ack_wr;
+
+   // -------------------------------------------------------------
+   // Taking WADDR from WCLK domain to RCLK domain
+   // -------------------------------------------------------------
+
+   // WADDR is latched into a stable register during the LOAD state for transfer to the RCLK domain.
+   assign waddr_latch_nxt = wr_fsm_load ? waddr : waddr_latch;
+
+   always_ff @(posedge wclk or negedge wrst_n) begin
+      if (~wrst_n) begin
+         waddr_latch <= '0;
+      end else begin
+         waddr_latch <= waddr_latch_nxt;
+      end
+   end
+
+   // Request the RCLK domain to sample WADDR when in REQ_ACK state.
+   riv_synchronizer_2ff i_waddr_load_cdc (
+       .src_in(wr_fsm_req_ack),
+       .dst_clk(rclk),
+       .dst_rst_n(rrst_n),
+       .dst_out(wr_fsm_req_ack_rd)
+   );
+
+   // Generate an acknowledge signal in the RCLK domain to indicate that WADDR has been successfully sampled.
+   always_ff @(posedge rclk or negedge rrst_n) begin
+      if (~rrst_n) begin
+         wr_fsm_recv_ack_rd <= 1'b0;
+      end else begin
+         wr_fsm_recv_ack_rd <= wr_fsm_req_ack_rd;
+      end
+   end
+
+   // Detect positive edge of wr_fsm_req_ack_rd
+   assign waddr_sample_rd = wr_fsm_req_ack_rd && ~wr_fsm_recv_ack_rd;
+
+   // WADDR is sampled in RCLK domain.
+   assign waddr_rd_nxt = waddr_sample_rd ? waddr_latch : waddr_rd;
+
+   always_ff @(posedge rclk or negedge rrst_n) begin
+      if (~rrst_n) begin
+         waddr_rd <= '0;
+      end else begin
+         waddr_rd <= waddr_rd_nxt;
+      end
+   end
+
+   // Get the acknowledge back into WCLK domain.  
+   riv_synchronizer_2ff i_waddr_ack_cdc (
+       .src_in(wr_fsm_recv_ack_rd),
+       .dst_clk(wclk),
+       .dst_rst_n(wrst_n),
+       .dst_out(wr_fsm_recv_ack)
+   );
+
+   // -------------------------------------------------------------
+   // Taking RADDR from RCLK domain to WCLK domain
+   // -------------------------------------------------------------
+
+   // RADDR is latched into a stable register during the LOAD state for transfer to the WCLK domain.
+   assign raddr_latch_nxt = rd_fsm_load ? raddr : raddr_latch;
+
+   always_ff @(posedge rclk or negedge rrst_n) begin
+      if (~rrst_n) begin
+         raddr_latch <= '0;
+      end else begin
+         raddr_latch <= raddr_latch_nxt;
+      end
+   end
+
+   // Request the WCLK domain to sample RADDR when in REQ_ACK state.
+   riv_synchronizer_2ff i_raddr_load_cdc (
+       .src_in(rd_fsm_req_ack),
+       .dst_clk(wclk),
+       .dst_rst_n(wrst_n),
+       .dst_out(rd_fsm_req_ack_wr)
+   );
+
+   // Generate an acknowledge signal in the WCLK domain to indicate that RADDR has been successfully sampled.
+   always_ff @(posedge wclk or negedge wrst_n) begin
+      if (~wrst_n) begin
+         rd_fsm_recv_ack_wr <= 1'b0;
+      end else begin
+         rd_fsm_recv_ack_wr <= rd_fsm_req_ack_wr;
+      end
+   end
+
+   // Detect positive edge of wr_fsm_req_ack_rd
+   assign raddr_sample_wr = rd_fsm_req_ack_wr && ~rd_fsm_recv_ack_wr;
+
+   // RADDR is sampled in WCLK domain.
+   assign raddr_wr_nxt = raddr_sample_wr ? raddr_latch : raddr_wr;
+
+   always_ff @(posedge wclk or negedge wrst_n) begin
+      if (~wrst_n) begin
+         raddr_wr <= '0;
+      end else begin
+         raddr_wr <= raddr_wr_nxt;
+      end
+   end
+
+   // Get the acknowledge back into RCLK domain. 
+   riv_synchronizer_2ff i_raddr_ack_cdc (
+       .src_in(rd_fsm_recv_ack_wr),
+       .dst_clk(rclk),
+       .dst_rst_n(rrst_n),
+       .dst_out(rd_fsm_recv_ack)
+   );
+
+endmodule  // riv_async_fifo_cdc
+
+`endif  // RIV_ASYNC_FIFO_CDC_SV
