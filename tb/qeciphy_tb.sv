e13e575b5493009b57628bb8bcdca53a52fade66
Gargi Sunil <gsunil@fanta-rhel.euhpc.riverlane.com>
2026-01-08 21:27:13 +0000

    Reverting GT parameter in tb

--- tb/qeciphy_tb.sv
+++ tb/qeciphy_tb.sv
@@ -17,7 +17,7 @@ module qeciphy_tb;
    `define msg_fatal(_str) $display("FATAL: (%0.2fus) %s", $realtime/1000.0, ``_str)
 
 `ifndef GT_TYPE
-   `define GT_TYPE "GTH"
+   `define GT_TYPE "GTY"
 `endif
 
    glbl glbl ();
