619752b5bb40b02b1c0f01a90538242d1c6059f1
rojalin mishra <rojalin.mishra@riverlane.com>
2026-01-22 12:04:13 +0000

    added another script to load the tools before running the regression

---
+++ ci/uvm_regression.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e 
+set -o pipefail
+
+echo "=== QECIPHY Multi-Platform UVM VCS Simulation ==="
+
+echo "Purging modules..."
+module purge
+
+echo "Loading Vivado and VCS..."
+module load xilinx/vivado/2024.1
+module load synopsys/vcs
+module load synopsys/verdi
+bash uvm/regression.sh
